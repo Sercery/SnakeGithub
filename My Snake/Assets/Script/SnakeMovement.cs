0abad3671bfedd6f19d4fa7ab68d442a5beeaf44
Sercery <158759140+Sercery@users.noreply.github.com>
2024-03-02 13:12:11 -0500

    Snake good

--- My Snake/Assets/Script/SnakeMovement.cs
+++ My Snake/Assets/Script/SnakeMovement.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using UnityEngine.SceneManagement;                       
 
 public class SnakeMovement : MonoBehaviour
 {
@@ -15,13 +16,17 @@ public class SnakeMovement : MonoBehaviour
     //bool goingRight;
 
     //body
+    public Transform bodyPrefab; //startomg the transformation of the prefab
+
+    List<Transform> segments;
 
 
                                             
     // Start is called before the first frame update
     void Start()
     {
-        //body
+        segments = new List<Transform>();       //create a list at the start of the game
+        segments.Add(transform);                //adding the head of the snake to the list. First hing on the lit is the head
     }
 
     // Update is called once per frame
@@ -48,11 +53,37 @@ public class SnakeMovement : MonoBehaviour
     }
 
     void FixedUpdate(){
+        for (int i = segments.Count -1; i> 0; i--) 
+        {
+            segments[i].position = segments[i - 1].position;
+        }
+
         transform.position = new Vector2
         (Mathf.Round(transform.position.x) + direction.x,
         Mathf.Round(transform.position.y) + direction.y);
     }
 
+    void Grow()
+    {
+        Transform segment = Instantiate(bodyPrefab);
 
+        segment.position = segments[segments.Count - 1].position;
+        segments.Add(segment);
+    }
+
+
+    void OnTriggerEnter2D (Collider2D other)
+    {
+        if(other.tag == "Food")
+        {
+            Debug.Log("hit");
+            Grow();
+        }
+
+        else if(other.tag == "Obstacle")
+        {
+            SceneManager.LoadScene("EndScene");
+        }
+    }
 
 }
